6127f7fa200cf01484f4aa91d5b162079f7e354f
Milinda Ranawaka <it18018288@my.sliit.lk>
2021-09-20 15:18:24 +0530

    Frontend Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Google cloud OAuth 2.0 Access
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Development
-        </a>
-      </header>
+      Google Drive OAuth 2.0 Project
+      Welcome
     </div>
   );
 }
