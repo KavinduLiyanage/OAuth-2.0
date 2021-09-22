8ca742e8c10cef07f62fd93716bb795b9c33047d
Milinda Ranawaka <it18018288@my.sliit.lk>
2021-09-22 14:28:07 +0530

    Read google drive function added

--- api/index.js
+++ api/index.js
@@ -72,6 +72,31 @@ app.post('/getUserInfo', (req, res) => {
     })
   });
 
+  //Read Drive using token
+app.post('/readDrive', (req, res) => {
+    if (req.body.token == null) return res.status(400).send('Token not found');
+    oAuth2Client.setCredentials(req.body.token);
+    const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+    drive.files.list({
+        pageSize: 10,
+    }, (err, response) => {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return res.status(400).send(err);
+        }
+        const files = response.data.files;
+        if (files.length) {
+            console.log('Files:');
+            files.map((file) => {
+                console.log(`${file.name} (${file.id})`);
+            });
+        } else {
+            console.log('No files found.');
+        }
+        res.send(files);
+    });
+  });
+
   
 //Server
 const PORT = process.env.PORT || 5000;
