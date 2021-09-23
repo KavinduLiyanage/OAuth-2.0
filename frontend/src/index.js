c0b9b2366be9c374e37ec02f336b4dc9903569d4
KavinduLiyanage <sandeepa8956@gmail.com>
2021-09-23 15:55:23 +0530

    Implemented login page with form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignInSide from './auth/login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SignInSide />
   </React.StrictMode>,
   document.getElementById('root')
 );
