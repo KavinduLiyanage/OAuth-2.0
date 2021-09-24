049ac8a5d13c29bd016450e5ce5e59708dcec34b
KavinduLiyanage <sandeepa8956@gmail.com>
2021-09-24 08:45:49 +0530

    Refactored mainContainer and homePage

--- frontend/src/containers/homePage.js
+++ frontend/src/containers/homePage.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 import ProductsList from "./Products/productsList";
 import { TOKEN_FNAME, TOKEN_LNAME } from "./config";
 
 export default function HomePage() {
-    return (
-        <div className="container">
-        <h1 align="center"> <span className="badge badge-dark">
-            Welcome {localStorage.getItem(TOKEN_FNAME)} {localStorage.getItem(TOKEN_LNAME)}
-        </span></h1>
-        <ProductsList/>
+  return (
+    <div className="container">
+      <h1 align="center">
+        {" "}
+        <span className="badge badge-dark">
+          Welcome {localStorage.getItem(TOKEN_FNAME)}{" "}
+          {localStorage.getItem(TOKEN_LNAME)}
+        </span>
+      </h1>
+      <ProductsList />
     </div>
-    )
+  );
 }
--- frontend/src/containers/mainContainer.js
+++ frontend/src/containers/mainContainer.js
@@ -4,23 +4,23 @@ import SignInSide from "../auth/login";
 // import PrivateRoute from "./ReactMiddleware/PrivateRoute";
 
 function MainContainer() {
-    return (
-        <React.Fragment>
-            <BrowserRouter>
-                    <Switch>
-                        <Route path = '/' component = { SignInSide }/>
-                        {/* <Route exact path = '/register' component = { Register }/> */}
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={SignInSide} />
+          {/* <Route exact path = '/register' component = { Register }/> */}
 
-                        {/*All the Private Routes of System*/}
-                        {/* <PrivateRoute component={HomePage} path="/" exact/>
+          {/*All the Private Routes of System*/}
+          {/* <PrivateRoute component={HomePage} path="/" exact/>
                         <PrivateRoute component={EditUser} path="/users/:id" exact/>
                         <PrivateRoute component={ProductAdd} path="/addProduct" exact/>
                         <PrivateRoute component={ProductList} path="/products" exact/>
                         <PrivateRoute component={ProductEdit} path="/products/edit/:id" exact/> */}
-                    </Switch> 
-            </BrowserRouter>
-        </React.Fragment>
-    );
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default MainContainer;
