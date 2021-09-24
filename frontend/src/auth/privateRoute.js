27cc2c490250056485c4b0bd0f0753a0134aad62
KavinduLiyanage <sandeepa8956@gmail.com>
2021-09-24 10:18:58 +0530

    Implemented private Route function for login users

---
+++ frontend/src/auth/privateRoute.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isLogin } from "./Auth";
+
+
+//All the private route Management
+/*
+Component - Component which should redirect to
+*/
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    return (
+        // Show the component only when the user is logged in
+        // Otherwise, redirect the user to /login page
+        <Route
+            {...rest}
+            render={(props) =>
+                isLogin() ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to="/login" />
+                )
+            }
+        />
+    );
+};
+
+export default PrivateRoute;
