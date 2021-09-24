5f84add98ddd92951f66712db034110e491a3ff3
KavinduLiyanage <sandeepa8956@gmail.com>
2021-09-24 08:42:00 +0530

    Updated access token getting function in frontend

--- frontend/src/auth/login.js
+++ frontend/src/auth/login.js
@@ -1,30 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import GoogleButton from 'react-google-button'
-import { display } from '@mui/system';
-import axios from 'axios';
-import queryString from 'query-string';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import GoogleButton from "react-google-button";
+import { display } from "@mui/system";
+import axios from "axios";
+import queryString from "query-string";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -38,31 +43,30 @@ export default function SignInSide() {
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
   const handleChange = () => {
-    axios.get(`http://localhost:5000/getAuthURL`)
-      .then(res => {
-        console.log(res);
-        window.location.replace(res.data);
-      })
+    axios.get(`http://localhost:5000/getAuthURL`).then((res) => {
+      console.log(res);
+      window.location.replace(res.data);
+    });
   };
 
-  
   useEffect(() => {
     console.log(location);
     console.log(queryString.parse(location.search));
     const article = { code: queryString.parse(location.search) };
-    axios.post('http://localhost:5000/getToken', article)
-        .then(response => console.log(response));
-  }, [location])
+    axios
+      .post("http://localhost:5000/getToken", article)
+      .then((response) => console.log(response));
+  }, [location]);
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -70,12 +74,14 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -83,18 +89,23 @@ export default function SignInSide() {
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
                 margin="normal"
                 required
@@ -127,10 +138,19 @@ export default function SignInSide() {
               >
                 Sign In
               </Button>
-              <div style={{display: 'flex', justifyContent: 'center', marginBottom:'15px'}}>
-                  <GoogleButton style={{width: '100%'}} onClick={() => handleChange()} />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginBottom: "15px",
+                }}
+              >
+                <GoogleButton
+                  style={{ width: "100%" }}
+                  onClick={() => handleChange()}
+                />
               </div>
-              
+
               <Grid container>
                 <Grid item xs>
                   <Link href="#" variant="body2">
