ecbb560f36a4c5721320f9691ffde0a2a8def7ed
KavinduLiyanage <sandeepa8956@gmail.com>
2021-09-27 14:27:27 +0530

    Completed file upload function using access token

--- frontend/src/containers/drive/fileUpload.js
+++ frontend/src/containers/drive/fileUpload.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DropzoneAreaBase } from "material-ui-dropzone";
+import Button from "@material-ui/core/Button";
+import { getToken } from "../../helpers/authHelper";
+import axios from "axios";
+import { serverUrl } from "../../configs/config";
 // import "./styles.css";
 
 export default function FileUpload() {
   const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    console.log(files);
+  }, [files]);
+
+  useEffect(() => {
+    console.log(getToken());
+  }, []);
 
   const handleAdd = (newFiles) => {
     newFiles = newFiles.filter(
@@ -16,11 +28,45 @@ export default function FileUpload() {
     setFiles(files.filter((f) => f !== deleted));
   };
 
+  const handleUpload = () => {
+    if (files.length === 0) return;
+    console.log("handleUpload");
+    const formDataArray = new FormData();
+
+    files.forEach((item) => {
+      formDataArray.append("file", item.file);
+    });
+
+    formDataArray.append("token", JSON.stringify(getToken()));
+
+    axios.post(`${serverUrl}/fileUpload`, formDataArray).then((response) => {
+      console.log(response);
+      if (response.status === 200) window.location.reload();
+    });
+  };
+
   return (
-    <DropzoneAreaBase
-      fileObjects={files}
-      onAdd={handleAdd}
-      onDelete={handleDelete}
-    />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        // height: '200px',
+      }}
+    >
+      <DropzoneAreaBase
+        fileObjects={files}
+        onAdd={handleAdd}
+        onDelete={handleDelete}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        onClick={handleUpload}
+      >
+        Upload
+      </Button>
+    </div>
   );
 }
