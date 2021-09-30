6db535740bef030d0e77e18f5a1a48f6fa8dfd9b
KavinduLiyanage <sandeepa8956@gmail.com>
2021-10-01 01:13:11 +0530

    Completed file view functionalities

--- frontend/src/containers/drive/viewDrive.js
+++ frontend/src/containers/drive/viewDrive.js
@@ -13,93 +13,107 @@ import DownloadIcon from "@mui/icons-material/Download";
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Stack from "@mui/material/Stack";
+import { toast } from "react-toastify";
 
 const fileDownload = require("js-file-download");
 
 export default function BasicTable() {
-  const [rowsNew, setRowsNew] = useState([]);
+  const [files, setFiles] = useState([]);
 
   useEffect(() => {
-    const body = { token: getToken() };
-    axios.post(`${serverUrl}/readDrive`, body).then((response) => {
-      console.log(response);
-      setRowsNew(response.data);
-    });
+    readDriveFiles();
   }, []);
 
+  // read drive files using access token
+  const readDriveFiles = () => {
+    const body = { token: getToken() };
+    axios.post(`${serverUrl}/readDrive`, body).then((response) => {
+      setFiles(response.data);
+    });
+  };
+
+  // download drive files using access token
   const handleDownload = (id) => {
-    console.log(id);
+    toast("File downloading!");
     const body = { token: getToken() };
     axios
       .post(`${serverUrl}/download/${id}`, body, { responseType: "blob" })
       .then((response) => {
-        console.log(response);
-        console.log(rowsNew.find((item) => item.id === id));
-        fileDownload(
-          response.data,
-          rowsNew.find((item) => item.id === id).name
-        );
+        if (response.status === 200) {
+          fileDownload(
+            response.data,
+            files.find((item) => item.id === id).name
+          );
+        } else toast("File downloading failed!");
       });
   };
 
+  
+  // delete drive files using access token
   const handleDelete = (id) => {
-    console.log(id);
+    toast("File delete request send!");
     const body = { token: getToken() };
     axios.post(`${serverUrl}/deleteFile/${id}`, body).then((response) => {
-      console.log(response);
+      if (response.status === 200) {
+        toast("File deleted successfully!");
+        readDriveFiles();
+      } else toast("File deleting failed!");
     });
   };
 
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>File Name</TableCell>
-            <TableCell align="center">Id</TableCell>
-            <TableCell align="right">Kind</TableCell>
-            <TableCell align="right">Mime Type</TableCell>
-            <TableCell align="center">Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        {rowsNew.length > 0 && (
-          <TableBody>
-            {rowsNew.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="center">{row.id}</TableCell>
-                <TableCell align="right">{row.kind}</TableCell>
-                <TableCell align="right">{row.mimeType}</TableCell>
-                <TableCell align="right">
-                  <Stack direction="row" spacing={2}>
-                    <Button
-                      id={row.id}
-                      onClick={(e) => handleDelete(e.target.id)}
-                      variant="outlined"
-                      startIcon={<DeleteIcon />}
-                    >
-                      Delete
-                    </Button>
-                    <Button
-                      id={row.id}
-                      onClick={(e) => handleDownload(e.target.id)}
-                      variant="contained"
-                      endIcon={<DownloadIcon />}
-                    >
-                      Download
-                    </Button>
-                  </Stack>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        )}
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>File Name</TableCell>
+              <TableCell align="center">Id</TableCell>
+              <TableCell align="right">Kind</TableCell>
+              <TableCell align="right">Mime Type</TableCell>
+              <TableCell align="center">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          {files.length > 0 && (
+            <TableBody>
+              {files.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="center">{row.id}</TableCell>
+                  <TableCell align="right">{row.kind}</TableCell>
+                  <TableCell align="right">{row.mimeType}</TableCell>
+                  <TableCell align="right">
+                    <Stack direction="row" spacing={2}>
+                      <Button
+                        id={row.id}
+                        onClick={(e) => handleDelete(e.target.id)}
+                        variant="outlined"
+                        startIcon={<DeleteIcon />}
+                      >
+                        Delete
+                      </Button>
+                      <Button
+                        id={row.id}
+                        onClick={(e) => handleDownload(e.target.id)}
+                        variant="contained"
+                        endIcon={<DownloadIcon />}
+                      >
+                        Download
+                      </Button>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          )}
+        </Table>
+      </TableContainer>
+    </>
   );
 }
